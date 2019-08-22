e1d458a3ca69e47c5a8e89abd36055d0aac6217d
Ирина <irbris@mail.ru>
2019-08-22 12:09:47 +0300

    Updates

---
+++ precache-manifest.cf493392d6a4485d8db184a6cd4fe081.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b0c19f833240749681f5",
+    "url": "/weather_app/static/js/runtime~main.b0c19f83.js"
+  },
+  {
+    "revision": "c5aa2e1136ca95dde2f2",
+    "url": "/weather_app/static/js/main.c5aa2e11.chunk.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/weather_app/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "c5aa2e1136ca95dde2f2",
+    "url": "/weather_app/static/css/main.5d61c99c.chunk.css"
+  },
+  {
+    "revision": "7d4fea372a22b5a957afc1f511c52ec5",
+    "url": "/weather_app/index.html"
+  }
+];
